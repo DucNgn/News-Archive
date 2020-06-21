288a5093fad497bad5b9dff9e2bba5c049808ea4
Duke Nguyen <triduc2311m@gmail.com>
2020-06-20 21:16:09 -0400

    Init Firebase project

---
+++ functions/index.js
@@ -0,0 +1,32 @@
+const functions = require('firebase-functions');
+const puppeteer = require('puppeteer');
+let browserPromise = puppeteer.launch({
+    args: [
+        '--no-sandbox',
+    ]
+});
+
+exports.helloWorld = functions.https.onRequest((req, res) => {
+ res .send("Hello");
+ 
+});
+
+exports.updateCNNScreenShot = async (req, res) => {
+    const url = 'https://www.cnn.com/';
+    const browser = await browserPromise;
+    const context = await browser.createIncognitoBrowserContext();
+    const page = await context.newPage();
+
+    await page.goto(url);
+
+    const image = await page.screenshot();
+
+    res.setHeader('Content-type', 'image/png');
+    res.send(image);
+};
+
+exports.updateInTime = functions.pubsub
+    .topic('Update news')
+    .onPublish(async message => {
+
+    });
