a07b6c855fee62a213b2a808327b8cd37e00ce4a
Duke Nguyen <triduc2311m@gmail.com>
2020-06-21 01:48:29 -0400

    Scraping the headlines of CNN
    + Server timed out multiple time

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,52 @@
 const functions = require('firebase-functions');
 const puppeteer = require('puppeteer');
+
+exports.helloWorld = functions.https.onRequest((req, res) => {
+ res.send("Hello");
+});
+
+// Test cloud function
+exports.randomNumber = functions.https.onRequest((request, response) => {
+    const number = Math.round(Math.random() * 100);
+    console.log(number)
+    response.send(number.toString());
+});
+
+exports.updateCNNScreenShot = functions.https.onRequest ((req, res) => {
+    let headlines;
+    try {
+        headlines = getCNNImg();
+    } catch(err) {
+        console.log("Error when scraping");
+    }
+    console.log("Got the headlines");
+    console.log(headlines.toString())
+   // res.send(headlines.toString());
+});
+
+
+
 let browserPromise = puppeteer.launch({
     args: [
         '--no-sandbox',
     ]
 });
 
-exports.helloWorld = functions.https.onRequest((req, res) => {
- res.send("Hello");
- 
-});
-
-exports.updateCNNScreenShot = async (req, res) => {
+const getCNNImg = async (req, res) => {
     const url = 'https://www.cnn.com/';
     const browser = await browserPromise;
+    console.info(browser);
     const context = await browser.createIncognitoBrowserContext();
     const page = await context.newPage();
 
-    await page.goto(url);
+    await page.goto(url, {waitUntil: 'networkidle2'});
 
-    const image = await page.screenshot();
+   /* const image = await page.screenshot();*/
+   /* const headlines = await page.evaluate(() => document.querySelector('cd__headline-text').innerText); */
+    const element = await page.$("cd__headline-text");
+    const headlines = await page.evaluate(element => element.textContent, element);
 
-    res.setHeader('Content-type', 'image/png');
-    res.send(image);
+    await context.close();
+    await browser.close();
+    return headlines;
 };
