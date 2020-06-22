866b658a473141bae4c6df610f8ac4db8d97024f
Tyler <shopping.nguyennota@gmail.com>
2020-06-22 11:27:50 +1000

    Dirty workspace

--- functions/index.js
+++ functions/index.js
@@ -50,3 +50,54 @@ const getCNNImg = async (req, res) => {
     await browser.close();
     return headlines;
 };
+
+
+// =================================
+// Thach's part
+// run firebase deploy --only functions:archive
+// DEBUGME: check function logs
+// Cloud Function(s) - archive
+// Cloud Function(s)
+exports.archive = functions.https.onRequest ((req, res) => {  
+    // Web Scrapping
+    let headlines;
+
+    try {
+        headlines = getCNNHeadlines();  // Get CNN headlines
+    } catch(err) {
+        console.log("Error when scraping: " + err);
+    }
+
+    console.log("Got the headlines");
+    console.log(headlines.toString())
+
+    res.send(headlines.toString());
+
+    // console.log("News archived");
+});
+
+
+// Local function(s)
+const getCNNHeadlines = async (req, res) => {
+const url = 'https://www.cnn.com/';
+const browser = await browserPromise;
+console.info(browser);
+
+const context = await browser.createIncognitoBrowserContext();
+const page = await context.newPage();
+
+await page.goto(url, {waitUntil: 'networkidle2'});
+
+/* const image = await page.screenshot();*/
+/* const headlines = await page.evaluate(() => document.querySelector('cd__headline-text').innerText); */
+const element = await page.$("cd__headline-text");
+const headlines = await page.evaluate(element => element.textContent, element);
+
+await context.close();
+await browser.close();
+
+return headlines;
+};
+
+
+// run firebase deploy --only functions:archive
--- functions/pubsub_schedule.js
+++ functions/pubsub_schedule.js
@@ -1,19 +1,54 @@
-import * as functions from 'firebase-functions';
+const functions = require('firebase-functions');
+const puppeteer = require('puppeteer');
 
 
+// Cloud Function(s)
+exports.archive = functions.https.onRequest ((req, res) => {  
+        // Web Scrapping
+        let headlines;
 
-exports.archive = functions.pubsub
-    .topic('news')
-    .onPublish(async (message, context) => {
-        console.log('The function was triggered at ', context.timestamp);
-        console.log('The unique ID for the event is ', context.eventId);
-        console.log('The name of message is ', message.attributes.name);
-        
-        // const news = await [puppeteer function];
+        try {
+            headlines = getCNNHeadlines();  // Get CNN headlines
+        } catch(err) {
+            console.log("Error when scraping: " + err);
+        }
 
-        console.log("News archived");
+        console.log("Got the headlines");
+        console.log(headlines.toString())
+
+        // console.log("News archived");
     });
 
- 
+
+// Puppeteer Browser Promise
+let browserPromise = puppeteer.launch({
+    args: [
+        '--no-sandbox',
+    ]
+});
+
+
+// Local function(s)
+const getCNNHeadlines = async (req, res) => {
+    const url = 'https://www.cnn.com/';
+    const browser = await browserPromise;
+    console.info(browser);
+
+    const context = await browser.createIncognitoBrowserContext();
+    const page = await context.newPage();
+
+    await page.goto(url, {waitUntil: 'networkidle2'});
+
+   /* const image = await page.screenshot();*/
+   /* const headlines = await page.evaluate(() => document.querySelector('cd__headline-text').innerText); */
+    const element = await page.$("cd__headline-text");
+    const headlines = await page.evaluate(element => element.textContent, element);
+
+    await context.close();
+    await browser.close();
+
+    return headlines;
+};
+
    
-// run firebase deploy --only functions
+// run firebase deploy --only functions:archive
